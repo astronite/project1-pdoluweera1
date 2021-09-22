e75bf48356538bd9f8da6fe315b8e8e48cc54d20
Praveen <praveenmcaa@gmail.com>
2021-09-21 21:25:46 -0400

    port fixes

--- app.py
+++ app.py
@@ -64,7 +64,10 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(
+        host = '0.0.0.0',
+        port = int(os.getenv("PORT", 8080)
+        debug = True)
 
 
 
