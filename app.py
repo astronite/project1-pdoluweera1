95eadb024399e024ca17ec1665b1bd24036ee25e
PraveenWSL <praveenmcaa@gmail.com>
2021-10-13 11:59:54 -0400

    Database setup for username + artist names

--- app.py
+++ app.py
@@ -7,9 +7,26 @@ import os
 import flask  
 from dotenv import find_dotenv, load_dotenv, main
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user 
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://kuzgfouwsjmldd:67464b0aa5c549c457e27fa4d938c9e5066373fdd48ed68516f0726887ca2697@ec2-3-215-83-124.compute-1.amazonaws.com:5432/dedpghnpirtebi"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+class Username(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    username = db.relationship('Artist', backref='username', lazy=True)
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    arist = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.Integer, db.ForeignKey('username.id'),
+        nullable=False)
+
+db.create_all()
 
 @app.route('/', methods = ['GET','POST'])
 
