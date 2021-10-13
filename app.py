42d22c3436a01b27c0eeec275ea869bced73be2d
PraveenWSL <praveenmcaa@gmail.com>
2021-10-13 08:38:27 -0400

    Made app start on login page with button that says signup, next will make button go to signup page

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 
+def login():
+    return flask.render_template("login.html")
+
 def index():
     a= songData()
     return flask.render_template("index.html", song = a[0], artist = a[1], image= a[2], preview = a[3], url = a[4])
 
-@app.route('/login')
 
-def login():
-    return flask.render_template("login.html")
 app.run(
     debug = True,
     host = '0.0.0.0',
